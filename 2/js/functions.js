5f9426a638abf22b990152283528a9979fe86573
Keks <keks@htmlacademy.ru>
2025-01-03 09:54:28 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,19 @@
+//Проверка длины строки.
+const checkText = function (sometext, maxSimbols) {
+  if (sometext.length <= maxSimbols) {
+    return true;
+  }
+  return false;
+};
+checkText();
+
+//Проверка полиндрома.
+const isPolindrom = function (string) {
+  const newString = string.replaceAll(' ','').toLowerCase();
+  let emptyString = '';
+  for (let i = newString.length - 1; i >= 0; i--) {
+    emptyString += newString[i];
+  }
+  return emptyString === newString;
+};
+isPolindrom();
