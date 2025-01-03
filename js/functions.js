ee5ff1a74e287824ec4da28a8eddc26ebee9a463
Келехсаев Владимир <cheseltinec@rambler.ru>
2025-01-03 13:47:16 +0700

    //Нужно еще больше функций!

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,19 @@
 //Проверка длины строки.
-const chekText = function (sometext, maxSimbols) {
+const checkText = function (sometext, maxSimbols) {
   if (sometext.length <= maxSimbols) {
     return true;
   }
   return false;
 };
+checkText();
 
 //Проверка полиндрома.
 const isPolindrom = function (string) {
-  let newString = string.replaceAll(' ','').toLowerCase();
+  const newString = string.replaceAll(' ','').toLowerCase();
   let emptyString = '';
   for (let i = newString.length - 1; i >= 0; i--) {
     emptyString += newString[i];
   }
   return emptyString === newString;
 };
+isPolindrom();
